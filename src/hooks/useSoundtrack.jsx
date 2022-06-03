0f1d9e7e03e68d9c69643dfa2f43b476850a021a
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-06-03 23:03:02 +0200

    fix bug with music

--- src/hooks/useSoundtrack.jsx
+++ src/hooks/useSoundtrack.jsx
@@ -1,27 +1,20 @@
 import { useEffect, useState } from "react";
-
 export default function useSountrack(music, volumeMusic) {
     const [audio, setAudio] = useState(new Audio(music));
-
     function changeAudio(e) {
         setAudio(new Audio(e));
     }
     useEffect(() => {
         audio.volume = volumeMusic;
-
         localStorage.setItem("volume", audio.volume);
     }, [volumeMusic]);
-
     useEffect(() => {
         audio.loop = true;
         audio.volume = volumeMusic;
-
         audio.play();
-
         return () => {
             audio.pause();
         };
     }, [audio]);
-
     return changeAudio;
 }
