20dd9c7ad490ca318562040a2718bc6e6fc0a903
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-06-05 12:13:23 +0200

    delete strict mode

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ const store = configureStore({
         }),
 });
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
