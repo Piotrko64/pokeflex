4993d49d48b4a23be840d96dbd5a939eba628abd
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-09-15 18:41:05 +0200

    change pokemon parameter

--- src/functions/someTesting/chooseRandomEnemyQG.spec.ts
+++
@@ -1,9 +0,0 @@
-import { it, expect } from "vitest";
-import { chooseRandomEnemy } from "../computerAI/chooseRandomEnemy";
-import { fakeEnemyArrayPokemons, fakeFriendArrayPokemons } from "./dataForTesting/dataChooseRandomEnemyQG";
-
-it("should return array with id: 'firstID' and 'secondID' ", () => {
-    const result = chooseRandomEnemy(fakeFriendArrayPokemons, fakeEnemyArrayPokemons);
-    const whatExpect = ["firstID", "secondID"];
-    expect(result).toEqual(whatExpect);
-});
